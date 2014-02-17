f51d8214dad12e80e26423dd2d9e67a3c5fed1f0
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-02-17 17:28:19 -0500

    Use d3-approved solution

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var divs = d3.selectAll('.chart div')
-divs.data([40,80,150,160,230,420])
-
-divs.
-  style('width', function(d, i){
-    console.log(d + 'px');
-    return d + 'px';
-  }).
-  html(function(d) { return d; });
-
-
+var data = [4, 8, 15, 16, 23, 42];
+// .enter().append('div') creates divs for each data point that did not have an
+// associated <div>.
+//
+// Think of the enter() selection as declaring the elements you *want* to exist
+// (but don't).
+d3.select('.chart').
+  selectAll('div').
+  data(data).
+  enter().append('div').
+  style('width', function(d) { return d*10 + 'px'; }).
+  text(function(d){ return d; });
