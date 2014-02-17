1bbded19fe3df398a63299b937bb8c33f88333c9
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-02-17 17:30:07 -0500

    Auto-linear scale

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var data = [4, 8, 15, 16, 23, 42];
+
+// Scale 0-42 into 0-420
+var scale = d3.scale.linear().
+  domain([0, d3.max(data)]).
+  range([0, 420]);
+
 // .enter().append('div') creates divs for each data point that did not have an
 // associated <div>.
 //
@@ -8,5 +14,5 @@ d3.select('.chart').
   selectAll('div').
   data(data).
   enter().append('div').
-  style('width', function(d) { return d*10 + 'px'; }).
+  style('width', function(d) { return scale(d) + 'px'; }).
   text(function(d){ return d; });
