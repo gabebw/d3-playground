38684440d7250569d6b597da5915104b8055e5d1
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-02-17 17:52:10 -0500

    d3 + svg = <3

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 var data = [4, 8, 15, 16, 23, 42];
 
-// Scale 0-42 into 0-420
+var width = 420,
+    barHeight = 20;
+
 var scale = d3.scale.linear().
   domain([0, d3.max(data)]).
-  range([0, 420]);
+  range([0, width]);
 
-// .enter().append('div') creates divs for each data point that did not have an
-// associated <div>.
-//
-// Think of the enter() selection as declaring the elements you *want* to exist
-// (but don't).
-d3.select('.chart').
-  selectAll('div').
-  data(data).
-  enter().append('div').
-  style('width', function(d) { return scale(d) + 'px'; }).
-  text(function(d){ return d; });
+var chart = d3.select('.chart').
+  attr('width', width).
+  attr('height', barHeight * data.length);
+
+// Add <g> elements for each data point
+var groupings = chart.selectAll('g').data(data).
+  enter().append('g').
+  attr('transform', function(d, i){
+    return 'translate(0, ' + i*barHeight + ')';
+  });
+
+// Add a bar for each <g>
+groupings.append('rect').
+  attr('width', scale).
+  attr('height', barHeight - 1);
+
+// Label each bar
+groupings.append("text").
+  attr("x", function(d) { return scale(d) - 3; }).
+  attr("y", barHeight / 2).
+  attr("dy", ".35em").
+  text(function(d) { return d; });
