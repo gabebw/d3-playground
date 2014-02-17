8cf22542e176691e39b15425e80a7bf844ec0fd8
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-02-17 18:03:34 -0500

    Use external TSV file

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-var data = [4, 8, 15, 16, 23, 42];
-
 var width = 420,
     barHeight = 20;
 
 var scale = d3.scale.linear().
-  domain([0, d3.max(data)]).
   range([0, width]);
 
 var chart = d3.select('.chart').
-  attr('width', width).
-  attr('height', barHeight * data.length);
+  attr('width', width);
 
-// Add <g> elements for each data point
-var groupings = chart.selectAll('g').data(data).
-  enter().append('g').
-  attr('transform', function(d, i){
-    return 'translate(0, ' + i*barHeight + ')';
-  });
+function createChart(error, data){
+  scale.domain([0,
+    d3.max(data, function(d) { return d.value; })]);
 
-// Add a bar for each <g>
-groupings.append('rect').
-  attr('width', scale).
-  attr('height', barHeight - 1);
+  chart.attr('height', barHeight * data.length);
 
-// Label each bar
-groupings.append("text").
-  attr("x", function(d) { return scale(d) - 3; }).
-  attr("y", barHeight / 2).
-  attr("dy", ".35em").
-  text(function(d) { return d; });
+  // Add <g> elements for each data point
+  var groupings = chart.selectAll('g').data(data).
+    enter().append('g').
+    attr('transform', function(d, i){
+      return 'translate(0, ' + i*barHeight + ')';
+    });
+
+  // Add a bar for each <g>
+  groupings.append('rect').
+    attr('width', function(d){ return scale(d.value); }).
+    attr('height', barHeight - 1);
+
+  // Label each bar
+  groupings.append("text").
+    attr("x", function(d) { return scale(d.value) - 3; }).
+    attr("y", barHeight / 2).
+    attr("dy", ".35em").
+    text(function(d) { return d.value; });
+}
+
+function type(d){
+  d.value = +d.value; // coerce to number
+  return d;
+}
+
+d3.tsv('data.tsv', type, createChart);
